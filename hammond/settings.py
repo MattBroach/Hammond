93abf45f4d7fa06f669303aee337fbba272d820b
Matt Broach <go.for.dover@gmail.com>
2014-06-04 16:20:06 -0400

    more secret key fixes

--- hammond/settings.py
+++ hammond/settings.py
@@ -7,11 +7,11 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-
+import os
 SECRET = os.environ.get('SECRET','my-default-secret-key')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
